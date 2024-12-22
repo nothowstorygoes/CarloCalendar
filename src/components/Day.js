6af23618ed0529ec548c38a43bb57f3fd982439c
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-12-22 13:12:12 +0100

    Truncated excessive events

--- src/components/Day.js
+++ src/components/Day.js
@@ -42,7 +42,7 @@ export default function Day({ day, rowIdx, currentMonthIdx, year, roundedClass }
 
   const displayEvents = dayEvents.slice(0, 2);
   if (dayEvents.length > 3) {
-    displayEvents.push({ title: `+${dayEvents.length - 2} more`, label: { color: "gray" } });
+    displayEvents.push({ title: `+${dayEvents.length - 2}`, label: { color: "gray" } });
   } else {
     displayEvents.push(...dayEvents.slice(2, 3));
   }
